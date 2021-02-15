1a7b9cd19a69601828aeeb871a7beae90164b0e1
Sas Iacob <sasiacobconstantin@yahoo.com>
2021-02-15 22:48:57 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace SOM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_drawLines = new System.Windows.Forms.Button();
+            this.btn_GenPct = new System.Windows.Forms.Button();
+            this.btn_Centri = new System.Windows.Forms.Button();
+            this.epoca_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(15, 14);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 738);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_drawLines
+            // 
+            this.btn_drawLines.Location = new System.Drawing.Point(857, 14);
+            this.btn_drawLines.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_drawLines.Name = "btn_drawLines";
+            this.btn_drawLines.Size = new System.Drawing.Size(109, 39);
+            this.btn_drawLines.TabIndex = 1;
+            this.btn_drawLines.Text = "Draw Lines";
+            this.btn_drawLines.UseVisualStyleBackColor = true;
+            this.btn_drawLines.Click += new System.EventHandler(this.btn_drawLines_Click_1);
+            // 
+            // btn_GenPct
+            // 
+            this.btn_GenPct.Location = new System.Drawing.Point(857, 74);
+            this.btn_GenPct.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_GenPct.Name = "btn_GenPct";
+            this.btn_GenPct.Size = new System.Drawing.Size(109, 49);
+            this.btn_GenPct.TabIndex = 2;
+            this.btn_GenPct.Text = "Genereaza Puncte";
+            this.btn_GenPct.UseVisualStyleBackColor = true;
+            this.btn_GenPct.Click += new System.EventHandler(this.btn_GenPct_Click);
+            // 
+            // btn_Centri
+            // 
+            this.btn_Centri.Location = new System.Drawing.Point(857, 140);
+            this.btn_Centri.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Centri.Name = "btn_Centri";
+            this.btn_Centri.Size = new System.Drawing.Size(109, 49);
+            this.btn_Centri.TabIndex = 3;
+            this.btn_Centri.Text = "Genereaza Neuroni";
+            this.btn_Centri.UseVisualStyleBackColor = true;
+            this.btn_Centri.Click += new System.EventHandler(this.btn_Centri_Click);
+            // 
+            // epoca_label
+            // 
+            this.epoca_label.AutoSize = true;
+            this.epoca_label.Location = new System.Drawing.Point(854, 222);
+            this.epoca_label.Name = "epoca_label";
+            this.epoca_label.Size = new System.Drawing.Size(68, 17);
+            this.epoca_label.TabIndex = 4;
+            this.epoca_label.Text = "Epoca : 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(981, 846);
+            this.Controls.Add(this.epoca_label);
+            this.Controls.Add(this.btn_Centri);
+            this.Controls.Add(this.btn_GenPct);
+            this.Controls.Add(this.btn_drawLines);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_drawLines;
+        private System.Windows.Forms.Button btn_GenPct;
+        private System.Windows.Forms.Button btn_Centri;
+        private System.Windows.Forms.Label epoca_label;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SOM
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        class Punct
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+            public Color color { get; set; }
+
+            public Punct(int x, int y, Color color)
+            {
+                this.x = x;
+                this.y = y;
+                this.color = color;
+
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        #region variabileGlobale
+        Graphics g;
+        Pen p;
+        Punct[] punct;
+        Punct[,] neuroni;
+        int n = 10000;
+        int[,] inputData;
+        int nr_epoci_propuse = 10;
+        #endregion
+
+       
+
+        private void btn_drawLines_Click_1(object sender, EventArgs e)
+        {
+           
+                g = panel1.CreateGraphics();
+                p = new Pen(Color.Black);
+                p.Width = 3;
+           
+           
+
+            g.TranslateTransform((float)(panel1.Width / 2), (float)(panel1.Height / 2));
+                drawLines();
+            
+        }
+        private void drawLines()
+        {
+
+
+
+            g.DrawLine(p, 0, 300, 0, -300);
+            g.DrawLine(p, -300, 0, 300, 0);
+        }
+
+        private void btn_GenPct_Click(object sender, EventArgs e)
+        {
+
+
+            punct = new Punct[n];
+            string path = "Input.txt";
+            inputData = new int[n, 2];
+            FileStream fisier;
+
+
+            if ((!File.Exists(path)) || (File.ReadLines(path).Count() != n))
+            {
+                fisier = File.Open(path, FileMode.Create);
+
+                StreamWriter sw = new StreamWriter(fisier);
+                for (int i = 0; i < n; i++)
+                {
+                  //  int x = getRandomNumber(-299, 300);
+                   // int y = getRandomNumber(-299, 300);
+                  //  sw.WriteLine(x + " " + y);
+
+                }
+                sw.Close();
+            } // creeaza fisier daca e nevoie.
+
+
+            string[] lines = File.ReadAllLines(path);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] words = lines[i].Split(' ');
+
+
+                punct[i] = new Punct(Convert.ToInt32(words[0]), Convert.ToInt32(words[1]), Color.Red); 
+
+
+
+
+
+
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+
+
+                DeseneazaPunct(i);
+
+            }
+
+        }
+
+        private void DeseneazaPunct(int i)
+        {
+            g.FillRectangle((new SolidBrush(punct[i].color)), punct[i].x, punct[i].y, 2, 2);
+        }
+        private void drawLines(Color c)
+        { int nr_neuroni = 10;
+            for (int i = 0; i < nr_neuroni; i++)
+            {
+                for (int j = 0; j < nr_neuroni; j++)
+                {
+                    p.Color = c;
+                    p.Width = 1;
+
+                    if (j < nr_neuroni - 1)
+                    {
+                        g.DrawLine(p, neuroni[i, j].x, neuroni[i, j].y, neuroni[i, j + 1].x, neuroni[i, j + 1].y);
+
+                    }
+                    if (i < nr_neuroni - 1)
+                    {
+                        g.DrawLine(p, neuroni[i, j].x, neuroni[i, j].y, neuroni[i + 1, j].x, neuroni[i + 1, j].y);
+
+                    }
+
+
+                }
+            }
+        }
+        private void btn_Centri_Click(object sender, EventArgs e)
+        {
+            int nr_neuroni = 10;
+          
+            int distanta =600 / nr_neuroni;
+            neuroni = new Punct[nr_neuroni,nr_neuroni];
+            for(int i = 0; i < nr_neuroni; i++)
+            {
+                
+                for (int j = 0; j<nr_neuroni;j++)
+                {
+
+                    neuroni[i, j] = new Punct((i * distanta) - 270, (j * distanta) - 270, Color.Blue); // normal
+                    //neuroni[i, j] = new Punct((i * distanta) - 270, 290, Color.Blue);   // dintr-o latura
+                   // neuroni[i, j] = new Punct(-290, -290, Color.Blue);   // dintr-un punct
+
+
+                    
+                    g.FillRectangle((new SolidBrush(neuroni[i,j].color)), neuroni[i, j].x, neuroni[i, j].y, 1, 1);
+                    
+                }
+            }
+
+            drawLines(Color.Blue);
+            Thread.Sleep(1500);
+            
+            Actualizeaza();
+        }
+
+
+        private double alpha(int t)
+        {
+            double rezultat = 0.7 * Math.Exp(-t / nr_epoci_propuse);
+                return rezultat;
+        }
+        private void Actualizeaza()
+        {
+           
+             int t = 1;
+            drawLines(Color.White);
+            double alfa = alpha(t);
+            double alfaV;
+            int vecinatate, maxX, minX, maxY, minY;
+
+            Punct index;
+            do
+            {
+                
+                drawLines(Color.White);
+                double putere = -((double)t / (double)nr_epoci_propuse);
+               
+                vecinatate = Convert.ToInt32(6.1 * Math.Exp(putere));
+                int i2, j2;
+                
+                for (int i = 0; i < n; i++)
+                {
+                    //drawLines(Color.White);
+                    index = findNearestNeuron(punct[i]);
+                    i2 = index.x;
+                    j2 = index.y;
+                    alfa = alpha(t);
+                    alfaV = alfa;
+
+                    minX = i2 - vecinatate;
+                    maxX = i2 + vecinatate;
+                    minY = j2 - vecinatate;
+                    maxY = j2 + vecinatate;
+
+                    minX = minX < 0 ? 0 : minX;
+                    maxX = maxX > 9 ? 9 : maxX;
+                    minY = minY < 0 ? 0 : minY;
+                    maxY = maxY > 9 ? 9 : maxY;
+
+
+                    for( i2 = minX; i2 <= maxX; i2++)
+                    {
+                        for( j2 = minY; j2 <= maxY; j2++)
+                        {
+                            neuroni[i2, j2].x = Convert.ToInt32(neuroni[i2, j2].x + alfa * (punct[i].x - neuroni[i2, j2].x));
+                            neuroni[i2, j2].y = Convert.ToInt32(neuroni[i2, j2].y + alfa * (punct[i].y - neuroni[i2, j2].y));
+                        }
+                    }
+
+                    
+                    
+
+                }
+
+
+
+
+
+                epoca_label.Text = $"Epoca: {t} \n Vecinatate: {vecinatate}";
+                epoca_label.Update();
+                t++;
+                drawLines(Color.Blue);
+                Thread.Sleep(500);
+
+                if (alfa < 0.014)
+                {
+                        drawLines();
+                    for (int i = 0; i < punct.Length; i++)
+                    {
+                        DeseneazaPunct(i);
+                    }
+                }
+            } while (alfa>0.01);
+
+            drawLines(Color.Blue);
+        }
+
+        private Punct findNearestNeuron(Punct punct)
+        {
+            double distantaFinala = 10000, distanta;
+            int nr_neuroni = 10;
+
+            Punct winner_index  = new Punct(0, 0, Color.Red);
+            for(int i = 0; i < nr_neuroni; i++)
+            {
+                for(int j = 0; j < nr_neuroni; j++)
+                {
+              distanta = Math.Sqrt(Math.Pow((punct.x - neuroni[i,j].x), 2) + Math.Pow((punct.y - neuroni[i,j].y), 2));
+
+
+                    if (distanta < distantaFinala)
+                    {
+                        distantaFinala = distanta;
+                        winner_index = new Punct(i, j, Color.Black);
+                    }
+                }
+
+            }
+
+            return winner_index; 
+        }
+    }
+    }
+
+
